adcfcf0b6b2dfd9e4c9e5b9a6c3d619ee29fbe7d
Freddi-rica <dibie134@gmail.com>
2025-10-29 18:22:04 +0100

    Added fullname.sh script with parameters

---
+++ fullname.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This script prints the user's full name and something about them
+
+first_name=$1
+last_name=$2
+something=$3
+
+echo "My full name is $first_name $last_name and I am $something."
+
